d751e11343ff4fe4c1c11a083c69877a8c773b42
Marc Bensan <marcbensan.inq@gmail.com>
2024-03-13 12:54:08 -0400

    completed home.html prototype

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: ['dark'],
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
 
