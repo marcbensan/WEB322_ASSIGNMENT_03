039dedb55baf6d951c5bca2e9e2ead750aac8ea6
Marc Bensan <marcbensan.inq@gmail.com>
2024-04-06 19:51:44 -0400

    added sequelize instance in server

--- server.js
+++ server.js
@@ -12,12 +12,26 @@
  *
  ********************************************************************************/
 
+require("dotenv").config();
 const legoData = require("./modules/legoSets");
 const express = require("express");
 const app = express();
 const path = require("path");
 const layout = require("express-ejs-layouts");
-require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD, 
+  {
+    host: process.env.PGHOST,
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+  }
+);
 
 // body parser
 app.use(express.json());
